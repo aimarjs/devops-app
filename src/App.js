bbfc686502426619b2defa197a8f86f132c8becb
Aimar <aimar@snapit.ee>
2018-05-15 14:45:02 +0200

    initial

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Devops test frontend application</h1>
+        </header>
+        <p className="App-intro">
+          <p>This is a test app</p>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
